90f59df3b58aebf6aa873d2d7ad55e148c5c2b83
Naval Shah <navalmaulikshah@gmail.com>
2024-03-11 00:04:30 -0400

    Finished Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => (
           <h1 className='flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]'>
             The Next <br className='sm:block hidden'/> {" "}
             <span className='text-gradient'> Variable </span> <br className='sm:block hidden' />
-            Payment Method
+            
           </h1>
 
           <div className='ss:flex hidden md:mr-4 mr-0'>
@@ -30,9 +30,24 @@ const Hero = () => (
           </div>
         </div>
 
-        <h1></h1>
+        <h1 className='font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full'>
+          Program Method
+        </h1>
+
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of experts(Naval haha) use a methodology to identify your needs to most likely fit your needs. We program websites for you</p>
 
       </div>
+
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+
+        {/* gradient start */}
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+        {/* gradient end */}
+      </div>
+
     </section>
   )
 
