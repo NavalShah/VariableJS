7c5cff82f63e825009d33c7ff5aa856466446c35
Naval Shah <navalmaulikshah@gmail.com>
2024-03-12 23:15:39 -0400

    Finished stat section, center

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -48,6 +48,10 @@ const Hero = () => (
         {/* gradient end */}
       </div>
 
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
+      </div>
+
     </section>
   )
 
