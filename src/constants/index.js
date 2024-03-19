22f57dbda10b3d4c379c9b71ee76d3d2366d17dd
Naval Shah <navalmaulikshah@gmail.com>
2024-03-18 21:20:57 -0400

    Finished About the founders section

--- src/constants/index.js
+++ src/constants/index.js
@@ -47,24 +47,24 @@ export const feedback = [
   {
     id: "feedback-1",
     content:
-      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
-    name: "Herman Jensen",
-    title: "Founder & Leader",
+      "CS Academy at SBHS",
+    name: "Naval Shah",
+    title: "Founder, CEO, Head of Tech",
     img: people01,
   },
   {
     id: "feedback-2",
     content:
       "Money makes your life easier. If you're lucky to have it, you're lucky.",
-    name: "Steve Mark",
-    title: "Founder & Leader",
+    name: "Vivek Banker",
+    title: "Founder, CMO, Head of Sales",
     img: people02,
   },
   {
     id: "feedback-3",
     content:
-      "It is usually people in the money business, finance, and international trade that are really rich.",
-    name: "Kenn Gallagher",
+      "I am a nerd, only like french",
+    name: "All of Vivek's friends",
     title: "Founder & Leader",
     img: people03,
   },
